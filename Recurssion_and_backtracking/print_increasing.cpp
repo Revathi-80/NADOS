b656cb28f63226110718b6089cbf8e30df281917
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-02-27 17:21:06 +0530

    Create print_increasing.cpp

---
+++ Recurssion_and_backtracking/print_increasing.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void printIncreasing(int n){
+   if(n==1){
+       cout<<"1"<<endl;
+       return;
+   }
+   printIncreasing(n-1);
+   cout<<n<<endl;
+    
+}
+
+
+int main(){
+    int n; cin>>n;
+    printIncreasing(n);
+}
